8d03bf55a61b49166c40b22e07a5191bedf33f85
MetroWind <chris.corsair@gmail.com>
2019-12-13 21:49:36 -0800

    Finish the makeIntEnum macro.

--- advent/day13.rs
+++ advent/day13.rs
@@ -1,71 +1,69 @@
+use std::stringify;
+use std::fmt;
 use crate::intcode::intcode;
 
 #[macro_export]
-macro_rules! makeEnum
+macro_rules! makeIntEnum
 {
     {
         $name:ident
         {
             $( $sub_name:ident $( = $x:literal )? ,)+
         } with $underlying_type:ty,
-        $( derive( $traits:tt ) )?
-    } => {$( #[derive( $traits )] )?
-          enum $name
-          {
-              $( $sub_name $( = $x )? ,)+
-          }
+        $( derive( $($traits:ident),+ ) )?
+    } => {
+        $( #[derive(fmt::Debug, $($traits),+) ] )?
+        enum $name
+        {
+            $( $sub_name $( = $x )? ,)+
+        }
 
-          impl $name
-          {
-              pub fn from(x: $underlying_type) -> Self
-              {
-                  match x
-                  {
-                      $( x if x == Self::$sub_name as $underlying_type => Self::$sub_name, )+
-                      _ => unreachable!()
-                  }
-              }
-          }
+        impl $name
+        {
+            pub fn from(x: $underlying_type) -> Result<Self, String>
+            {
+                match x
+                {
+                    $( x if x == (Self::$sub_name as $underlying_type) => Ok(Self::$sub_name), )+
+                        _ => Err(format!("Unknown convertion from {} to {}", x, stringify!($name))),
+                }
+            }
+        }
+
+        impl fmt::Display for $name
+        {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result
+            {
+                let represent = match self
+                {
+                    $( Self::$sub_name => stringify!($sub_name), )+
+                };
+                write!(f, "{}", represent)
+            }
+        }
     }
 }
 
-makeEnum!
+
+makeIntEnum!
 {
     TileType
     {
-        Empty,
-        Wall,
-    } with i32,
+        Empty = 0,
+        Wall = 1,
+        Block = 2,
+        Paddle = 3,
+        Ball = 4,
+    } with intcode::ValueType,
     derive(Copy, Clone)
-
-}
-// ////
-enum TileType
-{
-    Empty = 0,
-    Wall = 1,
-    Block = 2,
-    Paddle = 3,
-    Ball = 4,
-}
-
-impl TileType
-{
-    pub fn from(x: intcode::ValueType) -> Self
-    {
-        match x
-        {
-            x if x == Self::Empty as intcode::ValueType => Self::Empty,
-            x if x == Self::B as intcode::ValueType => x,
-            x if x == Self::C as intcode::ValueType => x,
-            _ => ...
-}    }
 }
 
 pub fn part1(input: &str) -> usize
 {
+    0
 }
 
 pub fn part2(input: &str) -> usize
 {
+    0
 }
